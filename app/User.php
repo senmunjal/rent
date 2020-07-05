451f57c06e215342cf9ebcf18e865b012df04dd6
senmunjal <senmunjal@gmail.com>
2020-07-05 23:24:14 +0530

    7/5

--- app/User.php
+++ app/User.php
@@ -8,7 +8,26 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    protected  $primaryKey = 'user_id';
+    protected  $primaryKey = 'id';
+
+    protected $fillable = [
+        'name',
+        'password',
+        'location',
+        'status',
+        'phoneno',
+        'rented_shop',
+        'profile_pic',
+        'aadhaar_no',
+        'email',
+    ];
+
+    protected $hidden = ['password', 'remeber_token'];
+
+    protected $cast = [
+        'email_verified_at' => 'datetime',
+        'phone_verified_at' => 'datetime'
+    ];
     
     public function shop()
     {
