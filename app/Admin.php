662902b4f1e92621db4b81b4cf050ee2fd0c27fb
senmunjal <senmunjal@gmail.com>
2020-07-05 23:25:16 +0530

    7/5

--- app/Admin.php
+++ app/Admin.php
@@ -3,8 +3,33 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Admin extends Model
 {
-    
+    use Notifiable;
+
+    protected $guard='admin';
+
+
+    protected $fillable = [
+        'name',
+        'password',
+        'permanent_address',
+        'current_address',
+        'status',
+        'phoneno',
+        'rented_shop',
+        'profile_pic',
+        'aadhaar_no',
+        'shop_count',
+        'email'
+    ];
+
+    protected $hidden = ['password', 'remeber_token'];
+
+    protected $cast = [
+        'email_verified_at' => 'datetime',
+        'phone_verified_at' => 'datetime'
+    ];
 }
